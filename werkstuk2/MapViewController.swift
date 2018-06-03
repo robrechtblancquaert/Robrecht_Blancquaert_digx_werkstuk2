8d4175947f7e73991a0c8fa012e77c837ffdd73d
student <student@students-mac.local>
2018-06-03 13:02:36 +0200

    Can load data from server into son, and parse the son

--- werkstuk2/MapViewController.swift
+++ werkstuk2/MapViewController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import CoreData
 
 class MapViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.getData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +22,49 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func getData() {
+        let url = URL(string: "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale")
+        let urlRequest = URLRequest(url: url!)
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: urlRequest) {
+            (data, response, error) in
+            guard error == nil else {
+                print("error calling GET")
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            DispatchQueue.main.async {
+                do {
+                   let json = try JSONSerialization.jsonObject(with: responseData, options: []) as? [AnyObject]
+                    self.loadJson(json: json!)
+                } catch let error as NSError {
+                    print(error)
+                }
+                
+            }
+        }
+        task.resume()
+    }
+    
+    func loadJson(json: [AnyObject]) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            else {
+                return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        for index in 0...json.count-1 {
+            let station = NSEntityDescription.insertNewObject(forEntityName: "Station", into: managedContext) as! Station
+            let object = json[index] as! [String: AnyObject]
+            print(object["number"] as! Int)
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
